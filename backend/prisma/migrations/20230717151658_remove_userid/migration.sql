2dec806799812a0ec75884e92546e0f9bf564131
keisuke ohashi <kog71903@gmail.com>
2023-07-18 00:17:24 +0900

    add userid

--- backend/prisma/migrations/20230717151658_remove_userid/migration.sql
+++ backend/prisma/migrations/20230717151658_remove_userid/migration.sql
@@ -2,14 +2,16 @@
   Warnings:
 
   - The primary key for the `UserToken` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - You are about to drop the column `userId` on the `UserToken` table. All the data in the column will be lost.
+  - You are about to drop the column `userId` on the `WinCode` table. All the data in the column will be lost.
   - Added the required column `id` to the `UserToken` table without a default value. This is not possible if the table is not empty.
 
 */
 -- AlterTable
 ALTER TABLE "UserToken" DROP CONSTRAINT "UserToken_pkey",
+DROP COLUMN "userId",
 ADD COLUMN     "id" TEXT NOT NULL,
-ALTER COLUMN "userId" DROP NOT NULL,
 ADD CONSTRAINT "UserToken_pkey" PRIMARY KEY ("id");
 
 -- AlterTable
-ALTER TABLE "WinCode" ALTER COLUMN "userId" DROP NOT NULL;
+ALTER TABLE "WinCode" DROP COLUMN "userId";
---
+++ backend/prisma/migrations/20230717151721_adduserid/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "UserToken" ADD COLUMN     "userId" TEXT;
+
+-- AlterTable
+ALTER TABLE "WinCode" ADD COLUMN     "userId" TEXT;
